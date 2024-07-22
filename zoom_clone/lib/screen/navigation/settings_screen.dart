dd98ceb3a346fbf752db88b6d74b9953808e35a0
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2024-07-22 09:55:12 +0530

    Update settings_screen.dart

--- zoom_clone/lib/screen/navigation/settings_screen.dart
+++ zoom_clone/lib/screen/navigation/settings_screen.dart
@@ -1,7 +1,9 @@
 
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:zoom_clone/resources/auth_methods.dart';
+import 'package:zoom_clone/screen/navigation/histry_meeting_screen.dart';
 import 'package:zoom_clone/widgets/custom_botton.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -13,18 +15,65 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   final AuthMethods _authMethods =AuthMethods();
-  
+  User? user = AuthMethods().auth.currentUser!;
+
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold( 
-      body: Column( 
-        children: [ 
-        
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column( 
+          children: [ 
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar( 
+                  radius: 60,
+                  backgroundImage: NetworkImage(
+                    user!.photoURL!
+                  ),
+                ),
+                const SizedBox(width: 30,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      user!.displayName!,
+                      style: TextStyle( 
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      user!.email!,
+                      style: TextStyle( 
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(height: 20,),
+            Text("Meeting History",
+              style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold ),
+            ),
+           SizedBox(
+            
+            height:400,
+            
+            child: HistryMeetingScreen())
+            // Text(key: ,)
+          ],
+        ),
       ),
       floatingActionButton:   CustomBotton(title: "Sign Out", onPressed: ()async{
-            await _authMethods.auth.signOut();
+           setState(() {
+               _authMethods.auth.signOut();
+           });
           }),
     );
   }
